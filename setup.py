faecd8084fb3bf545918e340dbe99781b777720b
Petr Pulc <pulcpetr@fit.cvut.cz>
2017-06-12 11:46:44 +0200

    Release version 0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Setup script for distutils, module majka.
 from distutils.core import setup, Extension
 
 setup(name='majka',
-      version='0.5',
+      version='0.6',
       description='Wrapper for Majka morphological analyser',
       long_description='''
 Native cPython binding to a C++ implementation of morphological analyser named Majka.
